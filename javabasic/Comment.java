9181f5bc7162a350a9915d79b940ada3c457f643
hyuk995-cloud <hyuk995@gmail.com>
2025-11-22 15:04:51 +0900

    day1

---
+++ javabasic/Comment.java
@@ -0,0 +1,15 @@
+package javabasic;
+
+public class Comment {
+    public static void main(String[] args) {
+        System.out.println("=== 주석 예제 ===");
+        System.out.println("이름 : 홍길동");
+        System.out.println("나이 : 31세");
+        // 이스케이프 문자 확인
+        System.out.println("줄 바꿈\n확인");
+        System.out.println("탭\t간격확인");
+        System.out.println("\" 큰 따옴표 출력");
+        System.out.println("\' 작은 따옴표 출력");
+        System.out.println("\\ 역 슬래시 \\ 출력");
+    }
+}
---
+++ javabasic/HelloWorld.java
@@ -0,0 +1,13 @@
+package javabasic;
+
+public class HelloWorld { /* 공개 클래스 선언, 파일명과 클래스명이 일치 */
+    // public (공개-어디서든 접근 가능), static (객체생성없이 호출)
+    // void (반환값 없음), String[] args : 명령행 인수 배열
+    /* 화면 메시지 출력 */
+    // System.out.println("Hello, World!")
+    // System.out.println("자바 프로그래밍을 시작합니다!")
+    public static void main(String[] args) {
+        System.out.println("Hellow, World");
+        System.out.println("자바 프로그래밍 시작합니다.!!!");
+    }
+}
