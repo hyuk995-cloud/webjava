caa010ea508aabde5f9134051888c9e9fc2724af
hyuk995-cloud <hyuk995@gmail.com>
2025-11-29 14:38:19 +0900

    day3

--- javabasicB/ScannerBasic.java
+++ javabasicB/ScannerBasic.java
@@ -1,30 +1,30 @@
-// package javabasicB;
+package javabasicB;
 
-// import java.util.Scanner; // Scanner class 호출
+import java.util.Scanner; // Scanner class 호출
 
-// public class ScannerBasic {
-//     public static void main(String[] args) {
-//         //Scanner객체 생성
-//         Scanner scanner = new Scanner(System.in);
+public class ScannerBasic {
+    public static void main(String[] args) {
+        //Scanner객체 생성
+        Scanner scanner = new Scanner(System.in);
         
-//         System.out.println("=== 정보 입력 ===");
+        System.out.println("=== 정보 입력 ===");
 
         
-//         System.out.println("이름 : ");
-//         String name =  scanner.next();
+        System.out.println("이름 : ");
+        String name =  scanner.next();
 
-//         System.out.println("나이 : ");
-//         int age =  scanner.nextInt();
+        System.out.println("나이 : ");
+        int age =  scanner.nextInt();
 
-//         System.out.println("키(cm) : ");
-//         double height =  scanner.nextDouble();
+        System.out.println("키(cm) : ");
+        double height =  scanner.nextDouble();
 
-//         System.out.println("\n ==== 입력 결과 =====");
-//         System.out.println("이름 : " + name);
-//         System.out.println("나이 : " + age + "세");
-//         System.out.println("키 : " + height);
+        System.out.println("\n ==== 입력 결과 =====");
+        System.out.println("이름 : " + name);
+        System.out.println("나이 : " + age + "세");
+        System.out.println("키 : " + height);
 
-//         scanner.close();
-//     }
+        scanner.close();
+    }
 
-// }
+}
